ffd5ef8ab68a1d816954b5ef2420f8a1eb5561a8
USER <sola201@naver.com>
2026-01-14 19:15:21 -0300

    feat: initial project setup with backend and frontend

---
+++ backend/src/main/java/com/mateandgit/candestore/CandestoreApplication.java
@@ -0,0 +1,13 @@
+package com.mateandgit.candestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandestoreApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mateandgit/candestore/CandestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateandgit.candestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
