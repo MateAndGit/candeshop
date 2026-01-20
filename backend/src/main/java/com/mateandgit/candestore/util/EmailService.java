9d9e128ecde06a13284a90173885d8a52f361de1
USER <sola201@naver.com>
2026-01-20 19:01:38 -0300

    fix "admin email"

--- backend/src/main/java/com/mateandgit/candestore/util/EmailService.java
+++ backend/src/main/java/com/mateandgit/candestore/util/EmailService.java
@@ -13,12 +13,12 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
     // 테스트용 내 이메일 변수로 고정
-    private final String MY_TEST_EMAIL = "candealvarez646@gmail.com";
+    private final String MY_TEST_EMAIL = "candealvarez444@gmail.com";
 
     @Async
     public void sendOrderConfirmation(String userEmail, Order order) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(MY_TEST_EMAIL); // 내 메일로 고정
+        message.setTo(userEmail); // 내 메일로 고정
         message.setSubject("[Cande Store] 주문 입금 안내");
         message.setText("총 금액: " + order.getTotalPrice() + "원\n계좌: [아르헨티나 계좌]");
         mailSender.send(message);
